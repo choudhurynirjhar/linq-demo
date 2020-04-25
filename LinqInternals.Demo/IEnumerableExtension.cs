a3c981f4959c33e02dadcb09d306a05a79bd8f21
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2020-04-25 18:38:09 -0400

    First check-in

---
+++ LinqInternals.Demo/IEnumerableExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqInternals.Demo
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<T> NewWhere<T>(this IEnumerable<T> items,
+            Func<T, bool> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ LinqInternals.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqInternals.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenItems = items.NewWhere(x => x % 2 == 0);
+            foreach (var item in evenItems)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}

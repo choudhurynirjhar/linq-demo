273d00c5848bd709d2872f9930b2728617c5c7ea
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2020-04-25 18:40:45 -0400

    Move Where demo to different method

--- LinqInternals.Demo/Program.cs
+++ LinqInternals.Demo/Program.cs
@@ -7,13 +7,18 @@ namespace LinqInternals.Demo
     {
         static void Main(string[] args)
         {
+            WhereDemo();
+
+        }
+
+        static void WhereDemo()
+        {
             var items = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var evenItems = items.NewWhere(x => x % 2 == 0);
             foreach (var item in evenItems)
             {
                 Console.WriteLine(item);
             }
-
         }
     }
 }

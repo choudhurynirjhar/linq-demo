6a78fd16022e4d45ec240e05cab13930ba52652c
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2020-05-23 15:02:50 -0400

    Add extension method demo

---
+++ LinqInternals.Demo/CustomerProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqInternals.Demo
+{
+    public class CustomerProcessor
+    {
+        public void PrintName(string customerName)
+        {
+            Console.WriteLine(customerName);
+        }
+    }
+}
---
+++ LinqInternals.Demo/StringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqInternals.Demo
+{
+    public static class StringExtension
+    {
+        public static string GiveFirstCharacter(this string item)
+        {
+            return item.Substring(0, 1);
+        }
+    }
+}
